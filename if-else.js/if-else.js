de3e66b23e5c875fe069d0ea1bd79f5d01b2fa60
Franzl76 <60292525+Franzl76@users.noreply.github.com>
2021-12-12 05:08:12 +0000

    Add files via upload

---
+++ if-else.js/if-else.js
@@ -0,0 +1,46 @@
+//Part 1:
+const age = 18;
+
+myAge = 10;
+
+if (myAge > age){
+
+console.log("you are welcome");
+
+}  else if (myAge < age){
+
+    console.log("kindly stay outside");
+}
+
+//Part 2:
+const sexFeMale = true;
+
+if (sexFeMale){
+
+    console.log("Welcome on the ladies night");
+}
+
+else if (sexMale){
+
+    console.log("Please visit us on our men's night")
+}
+
+//Part3:
+const driverStatusbob = true;
+
+if (driverStatusbob){
+
+    console.log("You are allowed to drive");
+}
+
+else if (driverStatusnotbob){
+
+    console.log("You are not allowed to drive, please call a taxi")
+}
+
+
+
+
+
+
+
